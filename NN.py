cc08e1ca0e0c5f4f9cf6312c7a5e0fbe16cdb5ff
anoburn <stephen.schaumann@arcor.de>
2018-09-01 14:23:16 +0200

    Modified layer structure/working

--- NN.py
+++ NN.py
@@ -1,65 +1,111 @@
-import numpy as np 
+import numpy as np
 import pickle
 
 def sigmoid(z):
     return np.array(1/(1+np.exp(-z)))
 
+def sigmoid_diff(z):
+    return sigmoid(z) * sigmoid(-z)
+
 def ReLU(z):
     z[np.where(z<0)] = 0
     return z
-   
+
+def ReLU_diff(z):
+    return np.where(z>0, 1, 0)
+
+activation_functions = {'ReLU': ReLU, 'sigmoid': sigmoid}
+derivative_functions = {'ReLU': ReLU_diff, 'sigmoid': sigmoid_diff}
+
 def load_NN( loadname):
-    return pickle.load( open( loadname , "rb" ) )    
+    return pickle.load( open( loadname , "rb" ) )
 
-class Layer(object): 
 
-    def __init__(self, neuronCount, neuronCountNext, layerType, activation_function): 
-        self.neuronCount = neuronCount 
-        if layerType == 'output':
-            self.weights = np.nan
-        else:
-            self.weights = np.random.rand(neuronCountNext,neuronCount)
-        self.layerType = layerType 
-        self.activation_function = activation_function 
-        
-    
-class NN(object): 
+class Layer(object):
 
-    def __init__(self, neuronCounts, activation_functions):
+    def __init__(self, n_input, neuronCount, activation_function):
+        self.neuronCount = neuronCount
+        self.weights = np.random.rand(n_input, neuronCount)
+        self.bias = np.random.rand(neuronCount)
+        self.activation_function = activation_functions[activation_function]
+
+    def forward(self, Input):
+        self.input = Input
+        #print("Input:", Input, "   weights:", self.weights.shape)
+        preactivations = Input @ self.weights + self.bias
+        result = self.activation_function(preactivations)
+        return result
+
+
+class ConvLayer(object):
+    """A convolutional layer with stride = 1 and max-pooling"""
+
+    def __init__(self, n_x_out, n_y_out, f_size):
+        #self.n_x = n_x_in
+        #self.n_y = n_y_in
+        self.n_x_out = n_x_out
+        self.n_y_out = n_y_out
+        self.f_size = f_size                  # filter size
+        self.filter = np.random.rand(f_size, f_size)
+
+    def forward(self, Input):
+        self.input = Input
+        n_x_in, n_y_in = Input.shape
+        result = np.empty((n_x_in - self.f_size + 1, n_y_in - self.f_size + 1))
+
+        for i in range(n_x_in - self.f_size + 1):
+            for j in range(n_y_in - self.f_size + 1):
+                region = Input[i : i + self.f_size, j : j + self.f_size]
+                result[i,j] = np.sum(region * self.filter)
+
+        result_pooled = np.empty((self.n_x_out, self.n_y_out))
+        step_x = result.shape[0] / self.n_x_out
+        step_y = result.shape[1]  /self.n_y_out
+        for i in range(self.n_x_out):
+            for j in range(self.n_y_out):
+                region = result[round(i*step_x) : round((i+1)*step_x),
+                                round(j*step_y) : round((j+1)*step_y)]
+                result_pooled[i,j] = np.max(region)
+        return result_pooled
+
+
+class NN(object):
+
+    def __init__(self, n_inputs, neuronCounts, activation_functions):
         layerCount = len(neuronCounts)
+        self.n_inputs = n_inputs
         self.neuronCounts = neuronCounts
-        self.layers = np.empty([layerCount], dtype = object)
-        self.layers[layerCount-1] = Layer(self.neuronCounts[layerCount-1],0,'output', activation_functions[layerCount-1])
-        for i in range(layerCount-2,0,-1):
-            self.layers[i] = Layer(self.neuronCounts[i],self.neuronCounts[i+1],'hidden',activation_functions[i])
-        self.layers[0] = Layer(self.neuronCounts[0],self.neuronCounts[1],'input', activation_functions[0])
+        self.layers = [None] * layerCount
+        self.layers[0] = Layer(n_inputs, neuronCounts[0], activation_functions[0])
+        for i in range(1, layerCount):
+            self.layers[i] = Layer(self.neuronCounts[i-1], self.neuronCounts[i], activation_functions[i])
 
     def run_NN(self, Input):
-        for index,layer in np.ndenumerate(self.layers):
-            if layer.activation_function =='ReLU':
-                if index[0] == 0:
-                    Output = ReLU(Input.T)
-                else:
-                    Output = ReLU(Output)
-            else:
-                if index[0] == 0:
-                    Output = sigmoid(Input.T)
-                else:
-                    Output = sigmoid(Output)
-            if index[0] < len(self.layers)-1:
-                Output =  layer.weights @ Output
-            else:
-                break
-        return Output
-        
+        result = Input
+        for layer in self.layers:
+            result = layer.forward(result)
+        #print(result)
+        return result
+
     def printNN(self):
         for i in range(len(self.neuronCounts)):
             print('Layer ',i)
-            print(self.layers[i].layerType)
+            #print(self.layers[i].layerType)
             print('weights:')
             print(self.layers[i].weights)
-            
+            print('bias:', self.layers[i].bias)
+
     def save_NN(self, savename):
         pickle.dump(self, open( savename , "wb" ))
-        
-    
+
+
+if __name__ == '__main__':
+    #network = NN(2, [2], ['ReLU'])
+    #network.printNN()
+    #result = network.run_NN([1, 1])
+    #print("Result:", result)
+    conv_layer = ConvLayer(5, 5, 3)
+    Input = np.random.rand(10,10)
+    output = conv_layer.forward(Input)
+    print(output)
+
--- Q_learning.py
+++ Q_learning.py
@@ -1,6 +1,6 @@
 import numpy as np
 import NN
-import Snake_Q 
+import Snake_Q
 
 def get_input( Snake , SnakeFieldSizeX, SnakeFieldSizeY):
     Input = np.zeros(6)
@@ -20,15 +20,15 @@ def get_input( Snake , SnakeFieldSizeX, SnakeFieldSizeY):
                     MinUp = min([MinUp , np.absolute(part[1]-yHead)])
                 else:
                     MinDown = min([MinDown , np.absolute(part[1]-yHead)])
-                    
+
             elif yHead == part[1]:
                 if part[0]-xHead < 0:
                     MinLeft = min([MinLeft , np.absolute(xHead-part[0])])
                 else:
                     MinRight = min([MinRight , np.absolute(xHead-part[0])])
         #print(MinLeft,MinUp,MinRight,MinDown)
-        
-        
+
+
         Input[0] = MinLeft
         Input[1] = MinUp
         Input[2] = MinRight
@@ -36,42 +36,42 @@ def get_input( Snake , SnakeFieldSizeX, SnakeFieldSizeY):
         Input[4] = np.absolute(Snake.snake[0][0]-Snake.food[0])
         Input[5] = np.absolute(Snake.snake[0][1]-Snake.food[1])
         return Input
-    
-    
-    
+
+
+
 class QLearning(object):
-    
-    def __init__(self, discount, learning_rate, epsilon_start, epsilon_max, epsilon_increase, memory_size, batch_size, SnakeFieldSizeX , SnakeFieldSizeY):  
+
+    def __init__(self, discount, learning_rate, epsilon_start, epsilon_max, epsilon_increase, memory_size, batch_size, SnakeFieldSizeX , SnakeFieldSizeY):
         self.discount         = discount
         self.learning_rate    = learning_rate                                   # alpha
         self.epsilon_start    = epsilon_start
         self.epsilon_max      = epsilon_max
-        self.epsilon_increase = epsilon_increase 
-        self.memory_size      = memory_size 
-        self.batch_size       = batch_size 
+        self.epsilon_increase = epsilon_increase
+        self.memory_size      = memory_size
+        self.batch_size       = batch_size
 
         self.SnakeFieldSizeX  = SnakeFieldSizeX
         self.SnakeFieldSizeY  = SnakeFieldSizeY
-        
-    def initialize_NN(self,neuronCounts, activation_functions):
-        self.Network = NN.NN(neuronCounts, activation_functions)
-  
 
-        
-        
-        
+    def initialize_NN(self, n_inputs, neuronCounts, activation_functions):
+        self.Network = NN.NN(n_inputs, neuronCounts, activation_functions)
+
+
+
+
+
     def train(self, runs):
-        Training_set = {"Old State" : [], "Current State": [], "reward": []}        
+        Training_set = {"Old State" : [], "Current State": [], "reward": []}
         run_time = 0
         TS_index = 0
         Snake = Snake_Q.SnakeQ(self.SnakeFieldSizeX,self.SnakeFieldSizeY)
-        
+
         while run_time < runs:
             score = 0
             Score = []
             Snake.start()
-            
-            
+
+
             while Snake.alive:
                 # Get Input for the NN
                 Input = get_input(Snake,self.SnakeFieldSizeX, self.SnakeFieldSizeY )
@@ -80,7 +80,7 @@ class QLearning(object):
                 #print('Input',Input)
                 Output = self.Network.run_NN(Input)
                 #print('Output',Output)
-                # Get direction (highest output value, 1 neuron left, second neuron up, 
+                # Get direction (highest output value, 1 neuron left, second neuron up,
                 # third neuron right, fourth neuron down)
                 key = np.where(Output== np.max(max(Output)))[0][0]
 
@@ -88,32 +88,32 @@ class QLearning(object):
                 Snake.move(self.Network, key)
                 if Snake.alive and Snake.score_old < Snake.score:
                     # Reward is 1 if our score increased
-                    r = 1 
+                    r = 1
                 elif Snake.alive:
                     # nothing happens score is decreased for punishing long ways
                     r = -0.1
                 elif Snake.alive== False:
                     # Score decreased if the snake died
                     r = -10
-                    
-                    
+
+
                 Input = get_input(Snake,self.SnakeFieldSizeX, self.SnakeFieldSizeY )
                 Training_set["Current State"].append(Input)
                 Training_set["reward"].append(r)
                 if TS_index == memory_size:
                     TS_index = 0
-            
+
                 if TS_index == batch_size:
                     train
-                    
+
                 TS_index += 1
             run_time += 1
-            
-            
+
+
             Score.append(Snake.score)
             print(np.mean(Score))
             print(Snake.score)
-    
+
 
 SnakeFieldSizeX  = 60
 SnakeFieldSizeY  = 60
@@ -127,10 +127,10 @@ memory_size      = 500
 batch_size       = 400
 
 a = QLearning(discount, learning_rate, epsilon_start, epsilon_max, epsilon_increase, memory_size, batch_size, SnakeFieldSizeX, SnakeFieldSizeY)
-a.initialize_NN(np.array([6,15,15,4]),['sigmoid','sigmoid','sigmoid','sigmoid'])
+a.initialize_NN(6, [15,15,4], ['sigmoid','sigmoid','sigmoid'])
 #Snake = Snake_Q.SnakeQ(60,60)
 #Snake.start()
 #Snake.move(a.Network)
- 
+
 a.train(runs)
-#a.printNN()  
+#a.Network.printNN()
